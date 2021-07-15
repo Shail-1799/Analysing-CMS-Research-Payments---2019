548287c249620b60a105d13bc9b65e2e7ac54a5a
Shail Shukla <64065292+Shail-1799@users.noreply.github.com>
2021-07-15 22:34:36 +0530

    Update Dash_variables.py

--- Dash_variables.py
+++ Dash_variables.py
@@ -7,7 +7,7 @@ import calendar
 import dash 
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_table 
 import dash_bootstrap_components as dbc
 
